5a056a606bec3f3a54def080ba76579b250fed65
ajielt7 <ajielt7@gmail.com>
2021-03-24 17:22:58 +0700

    nyoba

--- app/Views/admin/index.php
+++ app/Views/admin/index.php
@@ -9,26 +9,26 @@
         <h1 class="h3 mb-4 text-gray-800">User List</h1>
 
         <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Username</th>
-      <th scope="col">Email</th>
-      <th scope="col">Role</th>
-      <th scope="col">Action</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>$user</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-    </tr>
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Username</th>
+              <th scope="col">Email</th>
+              <th scope="col">Role</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>$user</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+            </tr>
     
-  </tbody>
-</table>
+          </tbody>
+        </table>
     </div>
 
 <?= $this->endSection(); ?>
