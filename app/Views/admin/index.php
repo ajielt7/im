c03fdb835ffbf61bb1c95569c4a770deb0b80263
ajielt7 <ajielt7@gmail.com>
2021-03-24 17:26:43 +0700

    sedikit nyoba

--- app/Views/admin/index.php
+++ app/Views/admin/index.php
@@ -3,7 +3,7 @@
 
 <?= $this->section('content'); ?>
 
-    <div class="container-fluid">
+  <div class="container-fluid">
 
         <!-- Page Heading -->
         <h1 class="h3 mb-4 text-gray-800">User List</h1>
@@ -29,6 +29,6 @@
     
           </tbody>
         </table>
-    </div>
+  </div>
 
 <?= $this->endSection(); ?>
